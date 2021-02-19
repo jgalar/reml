8657c7cc14ef9ea17c041ed3017a4d1cf5618a26
Jérémie Galarneau <jeremie.galarneau@gmail.com>
2021-02-19 14:51:47 -0500

    Transition project to use poetry
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use reml in a project::
-
-    import reml
--- tests/test_reml.py
+++ tests/test_reml.py
@@ -1,37 +1,5 @@
-#!/usr/bin/env python
-
-"""Tests for `reml` package."""
-
-import pytest
-
-from click.testing import CliRunner
-
-from reml import reml
-from reml import cli
+from reml import __version__
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'reml.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+def test_version():
+    assert __version__ == '0.1.0'
